8509dadec52bee1ac7ea99521050d0fab62b587c
Damian Wasilewski <wasyl.093@gmail.com>
2015-03-04 12:20:42 +0100

    Kontrolery i podstawowe viewsy...

---
+++ src/Shop/MovieBundle/Form/MovieType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Shop\MovieBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MovieType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('price')
+            ->add('category')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Shop\MovieBundle\Entity\Movie'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'shop_moviebundle_movie';
+    }
+}
