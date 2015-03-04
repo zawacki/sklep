869ed647c13673a5126800e4247ab3aafcd16fa8
Damian Wasilewski <wasyl.093@gmail.com>
2015-03-04 11:47:18 +0100

    Stworzenie klasy Review - opinie do filmow

---
+++ src/Shop/MovieBundle/Entity/Review.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Shop\MovieBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Review
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Review
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="review", type="text")
+     */
+    private $review;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="grade", type="integer")
+     */
+    private $grade;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set review
+     *
+     * @param string $review
+     * @return Review
+     */
+    public function setReview($review)
+    {
+        $this->review = $review;
+
+        return $this;
+    }
+
+    /**
+     * Get review
+     *
+     * @return string 
+     */
+    public function getReview()
+    {
+        return $this->review;
+    }
+
+    /**
+     * Set grade
+     *
+     * @param integer $grade
+     * @return Review
+     */
+    public function setGrade($grade)
+    {
+        $this->grade = $grade;
+
+        return $this;
+    }
+
+    /**
+     * Get grade
+     *
+     * @return integer 
+     */
+    public function getGrade()
+    {
+        return $this->grade;
+    }
+}
